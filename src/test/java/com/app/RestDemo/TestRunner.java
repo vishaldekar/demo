337a2726e69222bdf5145ee9a889207c41f7f511
vishaldekar <haldekarv@gmail.com>
2021-07-19 14:49:28 +0530

    updating Jenkinsfile

--- src/test/java/com/app/RestDemo/TestRunner.java
+++ src/test/java/com/app/RestDemo/TestRunner.java
@@ -3,10 +3,10 @@ package com.app.RestDemo;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
-        plugin = {"pretty", "html:target/report","json:target/report.json"})
+        plugin = {"pretty","json:target/cucumber.json"},
+        glue = "com.app.RestDemo.stepsdefinitions")
 public class TestRunner {
 }
