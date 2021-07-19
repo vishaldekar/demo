22f0acfdb71a383bca6df7b247b3aff16a68873b
vishaldekar <haldekarv@gmail.com>
2021-07-19 16:12:33 +0530

    some updates

--- src/test/java/com/app/RestDemo/Controller/CustomerControllerTest.java
+++ src/test/java/com/app/RestDemo/Controller/CustomerControllerTest.java
@@ -3,36 +3,25 @@ package com.app.RestDemo.Controller;
 import com.app.RestDemo.entity.Customer;
 import com.app.RestDemo.service.CustomerServiceWithRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@RunWith(Runner.class)
 class CustomerControllerTest {
 
 
--- src/test/java/com/app/RestDemo/RestDemoApplicationTests.java
+++ src/test/java/com/app/RestDemo/RestDemoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.app.RestDemo;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class RestDemoApplicationTests {
 
